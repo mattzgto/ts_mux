dc33efbc9396809c74d8a66ba523ae1197eb1e3c
Matt Zachary <matt@grsbd.com>
2017-07-28 20:38:18 +0000

    Added data

--- stream_mux.c
+++ stream_mux.c
@@ -6,29 +6,49 @@ int main ()
 {
 	// Constants
 	const char* vid_fifo = "video.fifo";
-	//const char* data_fifo = "data.fifo";
+	const char* data_fifo = "data.fifo";
 	const int num_vid = 1;
-	//const int num_data = 5;
-	const int packet_length = 100;
+	const int num_data = 5;
+	const int packet_length = 188;
 
 	// Counter
 	int i = 0;
+	int num_read = 0;
 
 	// Store what was read (1 byte at a time)
-	unsigned char* inbuffer;
+	unsigned char* inbuffer[188];
 
 	// Open the fifos
 	int vid_file;
-	//int data_file;
+	int data_file;
 	vid_file = open(vid_fifo, O_RDONLY);
-	//data_file = open(data_fifo , O_RDONLY | O_NONBLOCK);
+	data_file = open(data_fifo , O_RDONLY | O_NONBLOCK);
 
-	//while (1)
+	while (1)
 	{
-		for (i = 0; i < (num_vid * packet_length); i++)
+		// Read and output video packets (blocking)
+		for (i = 0; i < num_vid; i++)
 		{
-			read(vid_file, inbuffer, 1);
-			fprintf(stdout, "%.*s", 6, inbuffer);
+			num_read = 0;
+			while (num_read != packet_length)
+			{
+				num_read = read(vid_file, inbuffer, packet_length);
+			}
+			
+			fprintf(stdout, "%.*s", packet_length, inbuffer);
+			fflush(stdout);
+		}
+
+		// Read and output data packets (non-blocking)
+		for (i = 0; i < num_data; i++)
+		{
+			num_read = read(data_file, inbuffer, packet_length);
+			
+			if (num_read == packet_length)
+			{
+				fprintf(stdout, "%.*s", packet_length, inbuffer);
+				fflush(stdout);
+			}
 		}
 	}
 
